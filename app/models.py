86cd7fecfbdfb05b8004e34fd8008c16abb5e791
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-06-22 15:39:37 -0300

    Definindo modelo

---
+++ app/models.py
@@ -0,0 +1,10 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+# Definindo um modelo de dados para a tabela ScoreMQA, herdando de db.Model (classe base do Flask-SQLAlchemy)
+class ScoreMQA(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    nome_arquivo: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    score: so.Mapped[int] = so.mapped_column(index=True)
+    avaliado_em: so.Mapped[sa.DateTime] = so.mapped_column(index=True)
