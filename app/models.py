9f9ca25dc85c37b71b97d32ff2f9586594755e07
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-06-22 15:46:29 -0300

    Corrigindo datetime e iniciando respositorio de migraçoes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
@@ -7,4 +8,4 @@ class ScoreMQA(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     nome_arquivo: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     score: so.Mapped[int] = so.mapped_column(index=True)
-    avaliado_em: so.Mapped[sa.DateTime] = so.mapped_column(index=True)
+    avaliado_em: so.Mapped[datetime.datetime] = so.mapped_column(index=True)
