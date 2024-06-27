0b2f53bc59be02d4f988b9274a39915a7f3e162c
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-06-26 21:48:51 -0300

    Definição geral dos 3 endpoints

--- app/models.py
+++ app/models.py
@@ -9,3 +9,6 @@ class ScoreMQA(db.Model):
     nome_arquivo: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     score: so.Mapped[int] = so.mapped_column(index=True)
     avaliado_em: so.Mapped[datetime.datetime] = so.mapped_column(index=True, default=datetime.datetime.now(datetime.timezone.utc))
+
+    def __repr__(self) -> str:
+        return f'<ScoreMQA {self.nome_arquivo}: {self.score}>'
--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,18 @@ from app import app
 @app.route('/')
 def main_page():
     return 'Hello, World!'
+
+# Endpoint para realizar a avaliação do score de metadata de um dataset
+@app.route('/avaliar', methods=['POST'])
+def avaliar():
+    pass
+
+# Endpoint para listar os scores de metadata de todos os datasets avaliados
+@app.route('/avaliacoes', methods=['GET'])
+def avaliacoes():
+    pass
+
+# Endpoint para listar os scores de metadata de um dataset avaliado
+@app.route('avaliacoes/<dataset_id>', methods=['GET'])
+def avaliacao(dataset_id):
+    pass
