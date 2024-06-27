fff7404de6f11f0d15a44d456352b037230d2506
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-06-26 23:04:00 -0300

    Definindo métodos de serialização

--- app/models.py
+++ app/models.py
@@ -10,5 +10,20 @@ class ScoreMQA(db.Model):
     score: so.Mapped[int] = so.mapped_column(index=True)
     avaliado_em: so.Mapped[datetime.datetime] = so.mapped_column(index=True, default=datetime.datetime.now(datetime.timezone.utc))
 
+    # Método para transformar uma instância do modelo em dict, para depois ser enviado para o client como JSON
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'nome_arquivo': self.nome_arquivo,
+            'score': self.score,
+            'avaliado_em': self.avaliado_em.isoformat()
+        }
+    
+    # Método para transformar um dicionário em uma instância do modelo, para ser salva no banco de dados
+    def from_dict(self, data):
+        for field in ['nome_arquivo', 'score']:
+            if field in data:
+                setattr(self, field, data[field])
+
     def __repr__(self) -> str:
         return f'<ScoreMQA {self.nome_arquivo}: {self.score}>'
