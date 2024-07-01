620c69478ffb97d86dbc38963ad6e3a5533a88d8
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-07-01 15:08:23 -0300

    Definindo especificações OpenAPI em arquivos yaml

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+swagger = Swagger(app)
 
 from app import routes, models
