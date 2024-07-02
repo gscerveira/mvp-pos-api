6f0a7852cb27df59401e30aabed1d7db24d60b0e
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-07-02 18:58:24 -0300

    Implementação básica de CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flasgger import Swagger
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
